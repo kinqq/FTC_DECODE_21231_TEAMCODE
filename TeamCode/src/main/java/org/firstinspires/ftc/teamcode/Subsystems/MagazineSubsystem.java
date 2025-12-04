822883fba0454b5a6f3a0377201820c505819d6b
Minkyu <minkyu070602@gmail.com>
2025-12-03 22:31:18 -0800

    Optimize Magazine(Indexer) Subsystem
    pseudocode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MagazineSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MagazineSubsystem.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,65 +8,149 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 import com.seattlesolvers.solverslib.controller.PIDController;
 
-import org.firstinspires.ftc.teamcode.constant.ConstantsPIDF;
-import org.firstinspires.ftc.teamcode.constant.ConstantsServo;
-import org.firstinspires.ftc.teamcode.teleop.DriveMeet1;
+import org.firstinspires.ftc.teamcode.constant.Slot;
 
-public class MagazineSubsystem
-{
+public class MagazineSubsystem {
     private final CRServoImplEx magazine;
     private final AnalogInput analog;
     private final DcMotorEx encoder;
 
-    private int targetPos = 0;
+    private double targetDeg = 0;
+    private double offsetDeg = 0;
+    private Slot slot = Slot.FIRST;
+    public double TICKS_PER_DEG = 4000.0 * 4 / 360;
 
-    private boolean homed = false;
+    private final PIDController pid =
+        new PIDController(Constants.SERVO_P, Constants.SERVO_I, Constants.SERVO_D);
 
-
-    private PIDController pid = new PIDController(Constants.SERVO_P, Constants.SERVO_I, Constants.SERVO_D);
-    private PIDController pidHome = new PIDController(Constants.HOME_P, Constants.HOME_I, Constants.HOME_D);
-
-
-    public MagazineSubsystem(HardwareMap hwMap)
-    {
+    public MagazineSubsystem(HardwareMap hwMap) {
         magazine = hwMap.get(CRServoImplEx.class, "turntable");
         analog = hwMap.get(AnalogInput.class, "encoder");
         encoder = hwMap.get(DcMotorEx.class, "encoderDigital");
     }
 
-    public boolean home()
-    {
-        pidHome.setPID(Constants.HOME_P, Constants.HOME_I, Constants.HOME_D);
+    public void initialize() {
+        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        encoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        double voltage = analog.getVoltage();
-        double angleDeg = (voltage / 3.3) * 360.0;
+        offsetDeg = getAnalogDeg() + 30; // add 30 to compensate for servo offset
+        slot = Slot.FIRST;
 
-        double output = pidHome.calculate(angleDeg, 0);
-        magazine.setPower(output);
-
-        homed = angleDeg < 20;
-        if (homed) encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        return angleDeg < 20;
+        setSlotHold();
     }
 
-
-
-    public void update()
-    {
+    // Update
+    public void update() {
         pid.setPID(Constants.SERVO_P, Constants.SERVO_I, Constants.SERVO_D);
 
-        int currentPosition = encoder.getCurrentPosition();
+        int currentWorldTicks =
+            encoder.getCurrentPosition() + (int) Math.round(offsetDeg * TICKS_PER_DEG);
+        int targetWorldTicks = (int) Math.round(targetDeg * TICKS_PER_DEG);
 
-        double output = pid.calculate(currentPosition, targetPos);
-
-        magazine.setPower(Range.clip(output, -1, 1));
+        double output = pid.calculate(currentWorldTicks, targetWorldTicks);
+        magazine.setPower(Range.clip(output, -1.0, 1.0));
     }
 
-    public void setTarget(int target) {targetPos = target;}
+    //
+    // Public API
+    //
 
-    public int getTarget() {return targetPos;}
+    public void setSlotHold() {
+        setHoldTargetForCurrentSlot();
+    }
 
-    public int getEncoder() {return encoder.getCurrentPosition();}
-    public double getAnalog() {return (analog.getVoltage() / 3.3) * 360.0;}
-    public double getPower() {return magazine.getPower();}
+    public void setSlotHold(Slot slot) {
+        setSlot(slot);
+        setSlotHold();
+    }
+
+    public void setSlotLaunch() {
+        setLaunchTargetForCurrentSlot();
+    }
+
+    public void setSlotLaunch(Slot slot) {
+        setSlot(slot);
+        setSlotLaunch();
+    }
+
+    public void nextSlot() {
+        switch (slot) {
+            case FIRST:  setSlotHold(Slot.SECOND); break;
+            case SECOND: setSlotHold(Slot.THIRD);  break;
+            case THIRD:  setSlotHold(Slot.FIRST);  break;
+        }
+    }
+
+    public void prevSlot() {
+        switch (slot) {
+            case FIRST:  setSlotHold(Slot.THIRD);  break;
+            case SECOND: setSlotHold(Slot.FIRST);  break;
+            case THIRD:  setSlotHold(Slot.SECOND); break;
+        }
+    }
+
+    public double getTargetDeg() {
+        return targetDeg;
+    }
+
+    public double getEncoderDeg() {
+        return encoder.getCurrentPosition() / TICKS_PER_DEG;
+    }
+
+    public double getAnalogDeg() {
+        return (analog.getVoltage() / 3.3) * 360.0;
+    }
+
+    public double getPower() {
+        return magazine.getPower();
+    }
+
+    //
+    // Internal Math
+    //
+
+    private void setSlot(Slot slot) {
+        this.slot = slot;
+    }
+
+    private double holdDegForSlot(Slot s) {
+        switch (s) {
+            case FIRST:  return 0.0;
+            case SECOND: return 120.0;
+            case THIRD:  return 240.0;
+        }
+        return 0.0;
+    }
+
+    private double launchDegForSlot(Slot s) {
+        switch (s) {
+            case FIRST:  return 180.0;
+            case SECOND: return 300.0;
+            case THIRD:  return 60.0;
+        }
+        return 180.0;
+    }
+
+    private double getCurrentWorldDeg() {
+        double ticks = encoder.getCurrentPosition();
+        return ticks / TICKS_PER_DEG + offsetDeg;
+    }
+
+    // choose the closest 0/120/240 (+ n*360) to current angle
+    private void setHoldTargetForCurrentSlot() {
+        double current = getCurrentWorldDeg();
+        double base = holdDegForSlot(slot);
+
+        double k = Math.round((current - base) / 360.0);
+        targetDeg = base + 360.0 * k;
+    }
+
+    // same as above but for launch angles (180/300/60)
+    private void setLaunchTargetForCurrentSlot() {
+        double current = getCurrentWorldDeg();
+        double base = launchDegForSlot(slot);
+
+        double k = Math.round((current - base) / 360.0);
+        targetDeg = base + 360.0 * k;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveMeet2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveMeet2.java
@@ -1,30 +1,12 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.bylazar.configurables.PanelsConfigurables;
-import com.bylazar.telemetry.JoinedTelemetry;
-import com.bylazar.telemetry.PanelsTelemetry;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.AnalogInput;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.util.Range;
-import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.bylazar.configurables.annotations.Configurable;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Subsystems.*;
-import org.firstinspires.ftc.teamcode.subsystem.Turret;
-
-import dev.frozenmilk.sinister.loading.Pinned;
 
 @TeleOp(name = "Drive Meet 2")
-@Configurable
 public class DriveMeet2 extends OpMode
 {
     private TurretSubsystem turret;
@@ -48,31 +30,18 @@ public class DriveMeet2 extends OpMode
         turret = new TurretSubsystem(hardwareMap);
     }
 
-
-    boolean homed = false;
-
     @Override
     public void loop()
     {
-
-        if (!homed && magazine.home()) homed = true;
-        else if (homed) magazine.update();
-
-        if (gamepad1.aWasPressed()) magazine.setTarget(-1346);
-        if (gamepad1.bWasPressed()) magazine.setTarget(0);
-        if (gamepad1.xWasPressed())
-        {
-            homed = false;
-            magazine.setTarget(0);
-        }
+        if (gamepad1.startWasPressed()) magazine.initialize();
+        if (gamepad1.rightBumperWasPressed()) magazine.nextSlot();
+        if (gamepad1.leftBumperWasPressed()) magazine.prevSlot();
 
         turret.update();
 
-        telemetry.addData("Encoder Deg", magazine.getAnalog());
-        telemetry.addData("Encoder", magazine.getEncoder());
-        telemetry.addData("Target", magazine.getTarget());
+        telemetry.addData("Encoder Deg", magazine.getAnalogDeg());
+        telemetry.addData("Encoder", magazine.getEncoderDeg());
+        telemetry.addData("Target", magazine.getTargetDeg());
         telemetry.addData("Power", magazine.getPower());
     }
-
-
 }
